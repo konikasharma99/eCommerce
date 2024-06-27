93ba78491cb5ba59f7dee4d45c41f17f81499c4d
konika99 <konika@medialytics.pro>
2024-06-27 21:31:57 +0530

    Added search Prods api and frontend

--- Backend/index.js
+++ Backend/index.js
@@ -59,5 +59,16 @@ app.put("/products/:id", async (req, resp) => {
   );
   resp.send(result);
 });
+app.get("/search/:key", async (req, resp) => {
+  let result = await Product.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { price: { $regex: req.params.key } },
+      { company: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } },
+    ],
+  });
+  resp.send(result);
+});
 
 app.listen(5000);
--- front-end/src/components/AllProducts/AllProducts.js
+++ front-end/src/components/AllProducts/AllProducts.js
@@ -11,6 +11,18 @@ const AllProducts = () => {
     result = await result.json();
     setProducts(result);
   };
+  const onSearch = async (e) => {
+    let key = e.target.value;
+    if (key) {
+      let result = await fetch(`http://localhost:5000/search/${key}`);
+      result = await result.json();
+      if (result) {
+        setProducts(result);
+      }
+    } else {
+      getProducts();
+    }
+  };
   const deleteProd = async (id) => {
     let result = await fetch(`http://localhost:5000/products/${id}`, {
       method: "Delete",
@@ -26,6 +38,12 @@ const AllProducts = () => {
   return (
     <div className="dislayMain">
       <h1>All products</h1>
+      <input
+        type="text"
+        className="searchBar"
+        placeholder="Search"
+        onChange={(e) => onSearch(e)}
+      />
       <ul className="list-products-lable">
         <li className="list-label-per heading">S.No</li>
         <li className="list-label-per heading">Name</li>
@@ -34,21 +52,25 @@ const AllProducts = () => {
         <li className="list-label-per heading">Company</li>
         <li className="list-label-per heading">Operations</li>
       </ul>
-      {products.map((itm, index) => {
-        return (
-          <ul className="list-products-lable">
-            <li className="list-label-per">{index + 1}</li>
-            <li className="list-label-per">{itm.name}</li>
-            <li className="list-label-per">{itm.price}</li>
-            <li className="list-label-per">{itm.category}</li>
-            <li className="list-label-per">{itm.company}</li>
-            <li className="list-label-per">
-              <button onClick={() => deleteProd(itm._id)}>delete</button>
-              <Link to={"/updateProduct/" + itm._id}>Update</Link>
-            </li>
-          </ul>
-        );
-      })}
+      {products.length > 0 ? (
+        products.map((itm, index) => {
+          return (
+            <ul className="list-products-lable">
+              <li className="list-label-per">{index + 1}</li>
+              <li className="list-label-per">{itm.name}</li>
+              <li className="list-label-per">{itm.price}</li>
+              <li className="list-label-per">{itm.category}</li>
+              <li className="list-label-per">{itm.company}</li>
+              <li className="list-label-per">
+                <button onClick={() => deleteProd(itm._id)}>delete</button>
+                <Link to={"/updateProduct/" + itm._id}>Update</Link>
+              </li>
+            </ul>
+          );
+        })
+      ) : (
+        <h1>No Products found</h1>
+      )}
     </div>
   );
 };
